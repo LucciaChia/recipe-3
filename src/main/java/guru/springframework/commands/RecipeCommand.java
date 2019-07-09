c3717480ebe828c6d85658ba64ce1be80319cc8c
LucciaChia <krausovalucia23@gmail.com>
2019-07-09 12:29:42 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -0,0 +1,54 @@
+package guru.springframework.commands;
+
+import guru.springframework.domain.Difficulty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand {
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255) // in the DB by default, because we haven't specify any constraints there or
+    private String description; // mapping parameters Hibernate is going to create a bar chart column with length of 255
+                                // that's default behavior of Hibernate
+
+    @Min(1)
+    @Max(999)
+    private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
+    private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
+    private Integer servings;
+    private String source;
+
+    @URL // this will allow a blank => it can be blank
+    private String url;
+
+    @NotBlank
+    private String directions;
+
+    private Set<IngredientCommand> ingredients = new HashSet<>();
+    private Byte[] image;
+    private Difficulty difficulty;
+    private NotesCommand notes;
+    private Set<CategoryCommand> categories = new HashSet<>();
+}
